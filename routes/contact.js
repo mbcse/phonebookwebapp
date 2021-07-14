706e6c035b5d5a634aec570003c04a39b8ccb080
mbcse <mbcse50@gmail.com>
2021-07-14 21:19:46 +0530

    Added Database connection

---
+++ routes/contact.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', );
+router.post('/',);
+router.update('/');
+router.delete('/');
+
+
+
+module.exports = router;
